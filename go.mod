cb33e7f92acc9e8fb93b84796f9adf2157bc8280
Abdelrahman <abdelrahman@brown.edu>
2020-06-16 21:25:42 -0400

    integrate runner start request

--- go.mod
+++ go.mod
@@ -1,12 +1,23 @@
 module github.com/OpenROAD-Cloud/backend
 
+go 1.14
+
 require (
-	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker master
+	github.com/containerd/containerd v1.3.4 // indirect
+	github.com/docker/distribution v2.7.1+incompatible
+	github.com/docker/docker v0.0.0-20200612211812-aaf470eca7b5
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gin-gonic/gin v1.6.2
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/jhoonb/archivex v0.0.0-20180718040744-0488e4ce1681
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
+	google.golang.org/grpc v1.29.1 // indirect
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
 )
--- runner/handlers.go
+++ runner/handlers.go
@@ -1,10 +1,24 @@
 package runner
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/docker/distribution/uuid"
 
 	"github.com/gin-gonic/gin"
 )
+
+var flowDir = "/tmp/"
+
+func loadEnv() {
+	if value, exists := os.LookupEnv("FLOW_DIR"); exists {
+		flowDir = value
+	}
+}
 
 func statusResponseHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
@@ -13,8 +27,58 @@ func statusResponseHandler(c *gin.Context) {
 }
 
 func startResponseHander(c *gin.Context) {
-	// TODO: implement the flow runner
+	designFile, err := c.FormFile("design")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("design upload failed: %s", err.Error()),
+		})
+		return
+	}
+	sdcFile, err := c.FormFile("sdc")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("sdc upload failed: %s", err.Error()),
+		})
+		return
+	}
+	if dieArea := c.PostForm("die_area"); dieArea == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("missing die_area"),
+		})
+		return
+	}
+	if coreArea := c.PostForm("core_area"); coreArea == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("missing core_area"),
+		})
+		return
+	}
+
+	// Passed validation. Proceed ..
+	loadEnv()
+
+	// Create a directory for this run
+	runDir := uuid.Generate()
+	_ = os.MkdirAll(path.Join(flowDir, runDir.String()), os.ModePerm)
+
+	// Save files to this directory
+	filename := path.Join(flowDir, runDir.String(), filepath.Base(designFile.Filename))
+	if err := c.SaveUploadedFile(designFile, filename); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("couldn't process design file: %s", err.Error()),
+		})
+		return
+	}
+	filename = path.Join(flowDir, runDir.String(), filepath.Base(sdcFile.Filename))
+	if err = c.SaveUploadedFile(sdcFile, filename); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("couldn't process sdc file: %s", err.Error()),
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"status": "Not implemented yet",
+		"result_url": "http://<not-ready-yet>",
+		"message":    "OpenROAD flow has started. Check result_url!",
 	})
 }
